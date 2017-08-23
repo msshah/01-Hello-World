2024ea59196b6a42d58c46f4201f2d88cbdb585c
Mian S. Shah <safyanshah@gmail.com>
2017-08-23 12:42:04 +0500

    Add files via upload
    A simple program using C. Using printf statement to output strings to the terminal.

---
+++ HelloWorld.c
@@ -0,0 +1,33 @@
+/**
+  * WRITTEN BY 		: MIAN SAFYAN SHAH
+  *	CONTACT			: safyanshah@live.com
+  *
+  * LEVEL 			: BEGINNER
+  * ACCESS LEVEL 	: PUBLIC
+  *
+  * DESCRIPTION 	: This project has been started to help some of my friends learn a language.
+  *					  	My aim is to write programs in at least two languages C/C++ and JAVA.
+  *
+  *	ABOUT PROGRAM 	: THIS IS A HELLO WORLD PROGRAM WRITTEN IN C. THIS PROGRAM USES PRINTF TO OUTPUT STRING				
+  *
+  * COMPILING & RUN : On Windows, Use Turbo C/C++ OR Microsoft Visual C++ Environment
+  *					  On Mac OS and Linux Systems, use the following command using Terminal
+  *					  To compile : gcc HelloWorld.c -o HelloWorld
+  *					  To run the Program : ./HelloWorld
+  *				   	  If this command doesn't work, first change permissions of the Output file to executable by executing the following command
+  *					  chmod a+x HelloWorld
+  *					  and then run the program by typing ./HelloWorld
+  */
+
+#include <stdio.h>
+
+int main(void) {
+	
+	printf("Welcome to the World of C/C++. This is a Hello World Program.\n\n");
+	printf("This project has been started to help starters learn a language.\n");
+	printf("I will upload a single program in other languages too just to demonstrate\nthe working of same programs in other languages.\n");
+	
+	printf("\n\nStay Tuned\n\n");
+	printf("Thanks.\n");
+		
+}
