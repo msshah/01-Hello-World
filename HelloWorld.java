9733a72e3f2f250dbf9932525448c1697ed40841
Mian S. Shah <safyanshah@gmail.com>
2017-08-24 23:33:23 +0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,22 +1,24 @@
 /**
-	* Written by Mian Safyan Shah
-	* Contact : safyanshah@live.com
-	*
-	* This is a HelloWorld program written in Java.
-	*
-	* Use the command line to compile and run this program. It'll run in any IDE but 
-	* using command line is preferred.
-	*
-	* How to compile : Open Command Prompt on Windows or Terminal on Mac OS/Linux/Unix
-	* then navigate to the directory of this program and type the following command
-	*
-	* javac HelloWorld.java
-	*
-	* How to Run : after successful compilation, type java HelloWorld, you'll see output
-	* on the command line.
-	*
-	*
-  */
+  * Copyright (c) 2016 Mian Safyan Shah
+  *
+  * WRITTEN BY		: MIAN SAFYAN SHAH
+  * CONTACT		: safyanshah@live.com
+  *
+  * This is a HelloWorld program written in Java.
+  *
+  * Use the command line to compile and run this program. It'll run in any IDE but 
+  * using command line is preferred.
+  *
+  * How to compile : Open Command Prompt on Windows or Terminal on Mac OS/Linux/Unix
+  * then navigate to the directory of this program and type the following command
+  *
+  * javac HelloWorld.java
+  *
+  * How to Run : after successful compilation, type java HelloWorld, you'll see output
+  * on the command line.
+  *
+  *
+*/
 
 class HelloWorld
 {
